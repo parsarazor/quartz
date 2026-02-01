c4e149886bc098d29dee1e7cb0f4e7bd1d5be221
Parsa <prsamohamadi@gmail.com>
2026-02-02 03:08:49 +0330

    added new page and backlink-3

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,6 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       GitHub: "https://github.com/parsarazor",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
+      X: "https://twitter.com/parsamohamedi",
     },
   }),
 }
